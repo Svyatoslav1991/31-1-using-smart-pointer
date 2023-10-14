dd7395b4895769b8bb9d9ea6bd2f83f52887017f
Svyatoslav Myshkovskiy <svatoslav190391@yandex.ru>
2023-10-14 11:50:25 +0300

    Add block try-catch

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,28 @@ int main()
 {
 	std::cout << "\tTASK 31.1 USING SMART POINTER\n\n";
 
-	std::shared_ptr<Toy> toy = std::make_shared<Toy>("bone");
+	try
+	{
+		std::shared_ptr<Toy> toy = std::make_shared<Toy>("bone");
 
-	std::shared_ptr<Dog> dog1 = std::make_shared<Dog>("Fluffy", nullptr, 5);
-	std::shared_ptr<Dog> dog2 = std::make_shared<Dog>("Snowball", nullptr, 3);
+		std::shared_ptr<Dog> dog1 = std::make_shared<Dog>("Fluffy", nullptr, 5);
+		std::shared_ptr<Dog> dog2 = std::make_shared<Dog>("Snowball", nullptr, 3);
 
-	dog1->getToy(toy);
-	dog2->getToy(toy);
+		dog1->getToy(toy);
+		dog2->getToy(toy);
 
-	dog1->dropToy();
-	dog2->dropToy();
+		dog1->dropToy();
+		dog2->dropToy();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << "\n";
+	}
+	catch (...)
+	{
+		std::cout << "Something went wrong\n";
+	}
+	
 
 	return 0;
 }
